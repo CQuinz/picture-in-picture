7cfa32ffff1cf180ebdf3a83b6f29de9f8d4b6d1
Christian <christian.quinlan36@gmail.com>
2021-01-20 21:08:51 +0000

    Added eventListner and fuction to button to display the video picture in picture

--- script.js
+++ script.js
@@ -14,5 +14,14 @@ async function selectMediaStream(){
   }
 }
 
+button.addEventListener('click', async ()=>{
+  //Disable button
+  button.disabled = true;
+  //Start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset button
+  button.disabled = false;
+});
+
 // On load
 selectMediaStream();
