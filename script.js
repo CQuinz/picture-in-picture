1ab63440fa9e64735c7379dc6c68e43a674e845a
Christian <christian.quinlan36@gmail.com>
2021-01-20 20:44:03 +0000

    creating selectMediaFunction to get media stream

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
+async function selectMediaStream(){
+  try{
+
+  }catch(error){
+    console.log('We got an error', error);
+  }
+}
